98f859fbd5b734534763a6cf42e9f7422382e248
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-09-12 11:27:55 -0400

    Logged recipes.

--- savour/src/AddToListForm.js
+++ savour/src/AddToListForm.js
@@ -22,6 +22,7 @@ const AddToListForm = ({recipelist=false, setState=false, setToggleState=false, 
   // Recipe request.
   const recipeOpts = { method: "get", url: `/recipes`, data: {}, params: {}, headers };
   const [ recipeData ] = useAxios(recipeOpts);
+  console.log("RECIPE DATA LIST FORM", recipeData);
   // Occasion request.
   const OccOpts = { method: "get", url: `/occasions`, data: {}, params: {}, headers };
   const [ occasionData ] = useAxios(OccOpts);
